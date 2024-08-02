98697c7745b94ce7737d094fc387b39f031b6dbb
chuankuzideyun <yn88071@163.com>
2024-08-02 11:37:23 +0200

    Implementation of employee information paging query function

--- reggie_take_out/src/main/java/com/reggie/controller/EmployeeController.java
+++ reggie_take_out/src/main/java/com/reggie/controller/EmployeeController.java
@@ -1,12 +1,11 @@
 package com.reggie.controller;
 
 import java.time.LocalDateTime;
-
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.reggie.entity.Employee;
 import com.reggie.service.EmployeeService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 
@@ -83,5 +84,23 @@ public class EmployeeController {
         employeeService.save(employee);
 
         return R.success("新增员工成功");
+    }
+
+    //员工分页查询
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name){
+        log.info("page={},pageSize={}, name={}", page, pageSize, name);
+        
+        //分页构造器
+        Page pageInfo = new Page (page, pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = Wrappers.lambdaQuery();
+        //添加过滤条件
+        queryWrapper.like(org.apache.commons.lang.StringUtils.isNotEmpty(name), Employee::getName, name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        //执行查询
+        employeeService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
     }
 }
